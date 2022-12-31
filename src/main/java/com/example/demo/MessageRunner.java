180ff533e72757793e6dc02e5f843bd1f2dbeca8
Madhuleena2001 <madhuleenabanerjee2001@gmail.com>
2022-12-31 15:06:25 +0530

    COMMIT-003

--- src/main/java/com/example/demo/MessageRunner.java
+++ src/main/java/com/example/demo/MessageRunner.java
@@ -6,7 +6,7 @@ public class MessageRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to spring boot project");
 
 	}
 
