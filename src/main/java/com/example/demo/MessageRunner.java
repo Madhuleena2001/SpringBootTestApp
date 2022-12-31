88b3bbbda4324819bc4aff3819e294a8a3f5a165
Madhuleena2001 <madhuleenabanerjee2001@gmail.com>
2022-12-31 12:40:45 +0530

    Commit-002

---
+++ src/main/java/com/example/demo/MessageRunner.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
